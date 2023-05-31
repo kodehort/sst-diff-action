21bb50546f52b2367ead7ebfe0ac70bf385ae87d
alistairstead <alistair_stead@me.com>
2023-05-31 23:36:01 +0100

    wip: building project structure and configuring tools

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { debug, getInput, setFailed } from '@actions/core'
+import { initProject } from 'sst/project'
+import { writeSummary } from './diff'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const stage: string = getInput('stage')
+    // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    debug(`Running diff against stage ${stage}`)
+    await initProject({ stage })
+    await writeSummary()
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) setFailed(error.message)
   }
 }
 
